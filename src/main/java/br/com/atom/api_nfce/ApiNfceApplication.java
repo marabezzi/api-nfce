f3886b53e4191e4e6c76eb4275f35be7632e678c
Guilherme Clerice <61362302+marabezzi@users.noreply.github.com>
2025-10-29 10:43:39 -0300

    inicializacao do projeto

---
+++ src/main/java/br/com/atom/api_nfce/ApiNfceApplication.java
@@ -0,0 +1,13 @@
+package br.com.atom.api_nfce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiNfceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiNfceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/atom/api_nfce/ApiNfceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.atom.api_nfce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiNfceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
